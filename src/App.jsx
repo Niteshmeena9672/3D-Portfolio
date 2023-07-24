a4f28deb5f9697d9fd47167a1f16a3897cb1dc2b
nitesh <niteshmeena9672@gmail.com>
2023-07-25 00:02:23 +0530

    ball part completed

--- src/App.jsx
+++ src/App.jsx
@@ -16,8 +16,8 @@ const App = () => {
           </div>
         <About />
         <Experience />
-        <Tech />
         <Works />
+        <Tech />
         <Feedbacks />
         <div className='relative z-0'>
           <Contact />
